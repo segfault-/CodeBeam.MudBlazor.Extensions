d3ec9af96bc92c5abff9187be007fcf01e7572b7
segfault <five90-segfault@outlook.com>
2023-04-21 14:42:27 -0400

    navrail

---
+++ CodeBeam.MudBlazor.Extensions/Components/NavLinkM3/MudNavLinkM3.razor.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using MudBlazor;
+using MudBlazor.Interfaces;
+using MudBlazor.Utilities;
+
+namespace MudExtensions
+{
+    public partial class MudNavLinkM3 : MudBaseSelectItem, IDisposable
+    {
+        [Inject] private NavigationManager NavigationManager { get; set; }
+
+        private bool _hovered = false;
+        protected string Classname =>
+        new CssBuilder("mud-nav-item-m3")
+          .AddClass($"mud-ripple", !DisableRipple && !Disabled)
+          .AddClass("rounded")
+          .AddClass("mud-navlink-m3")
+          .Build();
+
+        protected string LinkClassname =>
+        new CssBuilder("mud-nav-link-m3 justify-center")
+          .AddClass($"mud-nav-link-disabled-m3", Disabled)
+          .Build();
+
+        protected string IconClassname =>
+        new CssBuilder("mud-nav-link-icon-m3")
+          .AddClass($"mud-nav-link-icon-default-m3", IconColor == Color.Default)
+          .Build();
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.LocationChanged += OnLocationChanged;
+            base.OnInitialized();
+        }
+        private void OnLocationChanged(object? sender, LocationChangedEventArgs args)
+        {
+            
+        }
+
+        protected Dictionary<string, object> Attributes
+        {
+            get => Disabled ? null : new Dictionary<string, object>()
+            {
+                { "href", Href },
+                { "target", Target },
+                { "rel", !string.IsNullOrWhiteSpace(Target) ? "noopener noreferrer" : string.Empty }
+            };
+        }
+
+        [Parameter] public EventCallback<MudNavLinkM3> NavLinkClicked { get; set; }
+        [Parameter]
+        [Category(CategoryTypes.NavMenu.Behavior)]
+        public string Label { get; set; }
+        /// <summary>
+        /// Icon to use if set.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.NavMenu.Behavior)]
+        public string Icon { get; set; }
+
+        /// <summary>
+        /// The color of the icon. It supports the theme colors, default value uses the themes drawer icon color.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.NavMenu.Appearance)]
+        public Color IconColor { get; set; } = Color.Default;
+
+        [Parameter]
+        [Category(CategoryTypes.NavMenu.Behavior)]
+        public NavLinkMatch Match { get; set; } = NavLinkMatch.Prefix;
+
+        [Parameter]
+        [Category(CategoryTypes.NavMenu.ClickAction)]
+        public string Target { get; set; }
+
+        /// <summary>
+        /// User class names when active, separated by space.
+        /// </summary>
+        [Parameter]
+        [Category(CategoryTypes.ComponentBase.Common)]
+        public string ActiveClass { get; set; } = "active";
+
+        [CascadingParameter] INavigationEventReceiver NavigationEventReceiver { get; set; }
+
+        protected Task HandleNavigation()
+        {
+            if (!Disabled && NavigationEventReceiver != null)
+            {
+                return NavigationEventReceiver.OnNavigation();
+            }
+            return Task.CompletedTask;
+        }
+
+
+        public override async Task OnClickHandler(MouseEventArgs ev)
+        {
+            await NavLinkClicked.InvokeAsync(this);
+            await base.OnClickHandler(ev);
+        }
+
+        public void Dispose()
+        {
+            NavigationManager.LocationChanged -= OnLocationChanged;
+        }
+
+    }
+}
