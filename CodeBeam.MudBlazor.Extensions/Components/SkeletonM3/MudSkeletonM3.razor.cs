5d6b58b94b7eaae267b79296375a32ab29b6405e
segfault <five90-segfault@outlook.com>
2023-06-09 17:12:46 -0400

    no message

---
+++ CodeBeam.MudBlazor.Extensions/Components/SkeletonM3/MudSkeletonM3.razor.cs
@@ -0,0 +1,13 @@
+﻿using MudBlazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MudExtensions
+{
+    public partial class MudSkeletonM3 : MudComponentBase
+    {
+    }
+}
