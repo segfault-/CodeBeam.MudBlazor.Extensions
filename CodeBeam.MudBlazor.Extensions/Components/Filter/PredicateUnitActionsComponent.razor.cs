d587720bc4cd42ffb86b3d71baaa49417dd05f86
segfault <five90-segfault@outlook.com>
2023-07-02 01:01:33 -0400

    no message

--- CodeBeam.MudBlazor.Extensions/Components/Filter/PredicateUnitActionsComponent.razor.cs
+++ CodeBeam.MudBlazor.Extensions/Components/Filter/PredicateUnitActionsComponent.razor.cs
@@ -1,15 +1,34 @@
 ﻿using Microsoft.AspNetCore.Components;
 using MudBlazor;
-using MudBlazor.Utilities;
 
 namespace MudExtensions
 {
-    public partial class PredicateUnitActionsComponent : MudComponentBase
+    public partial class PredicateUnitActionsComponent<T> : MudComponentBase
     {
         [Parameter] public EventCallback AddAtomicPredicateAsync { get; set; }
         [Parameter] public EventCallback AddCompoundPredicateAsync { get; set; }
         [Parameter] public EventCallback RemovePredicateUnitAsync { get; set; }
-        [Parameter] public bool IsCompoundPredicate { get; set; }
         [Parameter] public bool IsFirstElement { get; set; }
+
+        protected bool IsCompoundPredicate { get; set; }
+        protected bool IsAtomicPredicate { get; set; }
+        protected override void OnParametersSet()
+        {
+            base.OnParametersSet();
+            Console.WriteLine($"--> PredicateUnitActionsComponent<T> --> T={typeof(T)}");
+            IsAtomicPredicate = false;
+            IsCompoundPredicate = false;
+
+            if (typeof(T).IsAssignableFrom(typeof(AtomicPredicateComponent<>)) || (typeof(T).GetGenericTypeDefinition() == typeof(AtomicPredicateComponent<>)))
+            {
+                IsAtomicPredicate = true;
+            }
+
+            if (typeof(T).IsAssignableFrom(typeof(CompoundPredicateComponent<>)) || (typeof(T).GetGenericTypeDefinition() == typeof(CompoundPredicateComponent<>)))
+            {
+                IsCompoundPredicate = true;
+            }
+        }
+
     }
 }
