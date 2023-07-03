4b5e3cdead633311b805efc2795c0fd298cf68bb
segfault <five90-segfault@outlook.com>
2023-07-03 14:51:18 -0400

    no message

--- CodeBeam.MudBlazor.Extensions/Components/Filter/LogicalOperatorGutter/LogicalOperatorComponent.razor.cs
+++ CodeBeam.MudBlazor.Extensions/Components/Filter/LogicalOperatorGutter/LogicalOperatorComponent.razor.cs
@@ -3,6 +3,7 @@ using MudBlazor;
 
 namespace MudExtensions
 {
+#nullable enable
     public partial class LogicalOperatorComponent<T> : MudComponentBase
     {
         [Parameter] public uint Depth { get; set; }
@@ -10,6 +11,7 @@ namespace MudExtensions
         [Parameter] public CompoundPredicateLogicalOperator? ParentLogicalOperator { get; set; }
         [Parameter] public bool IsFirstElement { get; set; }
         [Parameter] public bool DisplayParentOperator { get; set; }
+        [Parameter] public RenderFragment? LogicalOperatorComponentTemplate { get; set; }
 
         protected bool IsCompoundPredicate => typeof(T).IsGenericType && typeof(T).GetGenericTypeDefinition() == typeof(CompoundPredicateComponent<>);
         protected bool IsAtomicPredicate => typeof(T).IsGenericType && typeof(T).GetGenericTypeDefinition() == typeof(AtomicPredicateComponent<>);
---
+++ CodeBeam.MudBlazor.Extensions/Components/Filter/LogicalOperatorGutter/LogicalOperatorTemplate.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace MudExtensions
+{
+#nullable enable
+    public partial class LogicalOperatorTemplate : MudComponentBase
+    {
+        [Parameter] public uint Depth { get; set; }
+        [Parameter] public CompoundPredicateLogicalOperator? ParentLogicalOperator { get; set; }
+        [Parameter, EditorRequired] public Func<int, bool>? TextContentCondition { get; set; }
+    }
+}
