37739cb324c67d2ed49ebe21fcfecc5dc7fc21e8
segfault <five90-segfault@outlook.com>
2023-07-13 16:48:59 -0400

    enum handling, still not correct, but better than before

--- CodeBeam.MudBlazor.Extensions/Components/Filter/AtomicPredicateConverter.cs
+++ CodeBeam.MudBlazor.Extensions/Components/Filter/AtomicPredicateConverter.cs
@@ -31,17 +31,32 @@ namespace MudExtensions
                     switch (propertyName)
                     {
                         case nameof(AtomicPredicate<T>.Value):
-                            atomicPredicate.Value = reader.GetString();
+                            var valueString = reader.GetString();
+
+                            // Check if the atomicPredicate.MemberType is not null and is Enum
+                            if (atomicPredicate.MemberType is not null && atomicPredicate.MemberType.IsEnum && valueString is not null)
+                            {
+                                // Parse the string value back to Enum
+                                atomicPredicate.Value = Enum.Parse(atomicPredicate.MemberType, valueString);
+                            }
+                            else
+                            {
+                                atomicPredicate.Value = valueString;
+                            }
+
                             break;
                         case nameof(AtomicPredicate<T>.Operator):
                             atomicPredicate.Operator = reader.GetString();
+                            break;
+                        case nameof(AtomicPredicate<T>.MultiSelectValues):
+                            atomicPredicate.MultiSelectValues = JsonSerializer.Deserialize<IEnumerable<string>>(ref reader);
                             break;
                         case nameof(AtomicPredicate<T>.Member):
                             atomicPredicate.Member = reader.GetString();
                             break;
                         case nameof(AtomicPredicate<T>.MemberType):
-                            var memberTypeString = reader.GetString();
-                            if(memberTypeString is not null)
+                            string? memberTypeString = reader.GetString();
+                            if (memberTypeString != null)
                             {
                                 atomicPredicate.MemberType = Type.GetType(memberTypeString);
                             }
@@ -52,6 +67,7 @@ namespace MudExtensions
 
             throw new JsonException();
         }
+
 
         public override void Write(Utf8JsonWriter writer, AtomicPredicate<T> value, JsonSerializerOptions options)
         {
